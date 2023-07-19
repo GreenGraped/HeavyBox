decb74a4f9b211f3b711f627b0e341d0aeeb395e
GreenGraped <121028592+GreenGraped@users.noreply.github.com>
2023-07-19 20:36:16 +0900

    initial commit

--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -2,15 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Functions : MonoBehaviour
+public class Functions
 {
     public static Functions f = new Functions();
-    public Vector2 V3toV2(Vector3 vector)
-    {
-        return new Vector2(vector.x, vector.y);
-    }
-    public Vector2 V2toV3(Vector2 vector, float z)
-    {
-        return new Vector3(vector.x, vector.y, z);
-    }
+    public Vector2 V3toV2(Vector3 vector) => new Vector2(vector.x, vector.y);
+    public Vector2 V2toV3(Vector2 vector, float z) => new Vector3(vector.x, vector.y, z);
 }
--- Assets/Scripts/Player_move.cs
+++ Assets/Scripts/Player_move.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Player_move : MonoBehaviour
 {
-    Vector2 input;
+    Vector2 movementInput;
+    [SerializeField] Vector2 temp_Input = Vector2.zero;
     Transform target;
     [SerializeField] bool input_exist;
+    [SerializeField] float movement_speed;
     void Start()
     {
         input_exist = false;
@@ -17,28 +19,44 @@ public class Player_move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /* input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if (input == Vector2.zero)
-        {
-            transform.Find("to_move").position = transform.position;
-        } else
-        {
-            transform.Find("to_move").position = Functions.f.V3toV2(transform.position) + input;
-        } */
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //if (Input.anyKey && !input_exist)
+        //{
+        //    if (movementInput.x != 0 && movementInput.y != 0)
+        //    {
+        //        movementInput.x = 0;
+        //    }
+        //    target.position = Functions.f.V3toV2(transform.position) + (movementInput * movement_speed);
+        //    input_exist = true;
+        //}
+        //else if (Input.anyKey == false && input_exist)
+        //{
+        //    target.position = transform.position;
+        //    input_exist = false;
+        //}
 
-        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if (input != Vector2.zero && !input_exist)
+        //if (input_exist)
+        //{
+        //    transform.position = target.position;
+        //    target.position = transform.position;
+        //    // input_exist = false;
+        //}
+        if (Input.anyKey && movementInput != temp_Input)
         {
-            if (input.x != 0 && input.y != 0)
+            if (movementInput.x != 0 && movementInput.y != 0)
             {
-                input.x = 0;
+                movementInput.x = 0;
             }
-            target.position = Functions.f.V3toV2(transform.position) + input;
-            input_exist = true;
-        } else if (input == Vector2.zero && input_exist)
+            temp_Input = movementInput;
+            target.position = Functions.f.V3toV2(transform.position) + (movementInput * movement_speed);
+            transform.position = target.position;
+            target.position = transform.position;
+        }
+        else if (!Input.anyKey)
         {
             target.position = transform.position;
-            input_exist = false;
+            temp_Input = Vector2.zero;
         }
+
     }
 }
